10cb25d57e5fcd3ade6594e3ba8221769e01448e
Theredwiking <hjulmandn@gmail.com>
2021-10-21 08:52:19 +0200

    A basic working navbar

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text>Home Screen</Text>
+            <Button title="Go to Details" onPress={() => navigation.navigate('Details', {itemId: 86, otherParam: 'anything you want here'})} />
+            <StatusBar style="auto" />
+        </View>
+    )
+}
+
+function DetailScreen({ route, navigation }) {
+    const { itemId, otherParam } = route.params;
+    return (
+        <View style={styles.container}>
+            <Text>Details Screen</Text>
+            <Text>itemId: {JSON.stringify(itemId)}</Text>
+            <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+            <Button title="Go to Details... Again" onPress={() => navigation.push('Details')} />
+            <Button title="Go to Home" onPress={() => navigation.navigate('Home') } />
+            <Button title="Go back" onPress={() => navigation.goBack()} />
+            <Button title="Go back to first screen in stack" onPress={() => navigation.popToTop()} />
+            <StatusBar style="auto" />
+        </View>
+    )
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }}/>
+                <Stack.Screen name="Details" component={DetailScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
